9dae08cdc99fbfa52d31ac6ddd4a8126f8c8cf85
rayan1242 <rayyanmaindargi12@gmail.com>
2024-06-24 14:00:01 +0530

    completed book store

--- frontend/src/pages/ShowBooks.jsx
+++ frontend/src/pages/ShowBooks.jsx
@@ -8,28 +8,51 @@ const ShowBooks = () => {
   const [loading,setLoading] = useState(false);
   const {id} = useParams();
 
-useEffect(()=>{
-  setLoading(true);
-  axios
-    .get(`http://localhost:5555/books/${id}`)
-    .then((response)=>{
-      setBook(response.data);
-      setLoading(false);
-    })
-    .catch((error)=>{
-      console.log(error);
-      setLoading(false);
-    })
-})  
+  useEffect(()=>{
+    setLoading(true);
+    axios
+      .get(`http://localhost:5555/books/${id}`)
+      .then((response)=>{
+        setBook(response.data);
+        setLoading(false);
+      })
+      .catch((error)=>{
+        console.log(error);
+        setLoading(false);
+      })
+  },[])  
   return (
     <div className='p-4'>
       <BackButton/>
       <h1 className='text-3xl my-4'>Show Book</h1>
       {loading ? (
-        <Spinner />
+        <Spinner/>
       ) : (
         <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
-
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Id</span>
+            <span>{book._id}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>title</span>
+            <span>{book.title}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>author</span>
+            <span>{book.author}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>publishYear</span>
+            <span>{book.publishYear}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Create Time</span>
+            <span>{new Date(book.createdAt).toString()}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 text-gray-500'>Last Update Time</span>
+            <span>{new Date(book.updatedAt).toString()}</span>
+          </div>
         </div>
       )}
     </div>
